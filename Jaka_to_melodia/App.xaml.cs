dbb8c93f6f26b1aeda5479933647ab9bd3c7f799
kcecherz <s61276@student.ubb.edu.pl>
2026-03-10 21:20:52 +0100

    WPF-Start

---
+++ Jaka_to_melodia/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Jaka_to_melodia
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
