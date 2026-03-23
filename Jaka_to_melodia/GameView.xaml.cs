b95d7c4bef4ae3e7bf1a7025c11ea9bcb7160578
bpekala354 <bpekala354@gmail.com>
2026-03-23 22:10:16 +0100

    testowy odtwarzacz muzyki

--- Jaka_to_melodia/GameView.xaml.cs
+++ Jaka_to_melodia/GameView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media; 
 
 namespace Jaka_to_melodia
 {
@@ -7,17 +10,64 @@ namespace Jaka_to_melodia
     {
         private Profile _currentPlayer;
 
+        private MediaPlayer _mediaPlayer;
+        private List<Song> _songs;
+        private Random _random;
+        private Song _currentSong; 
+
         public GameView(Profile player)
         {
             InitializeComponent();
-
             _currentPlayer = player;
+            TxtPlayerName.Text = "Gra: " + _currentPlayer.Name;
 
-            TxtPlayerName.Text = _currentPlayer.Name;
+            _mediaPlayer = new MediaPlayer();
+            _random = new Random();
+
+            LoadAndPlayMusic();
+        }
+
+        private void LoadAndPlayMusic()
+        {
+            SettingsManager settingsManager = new SettingsManager();
+            AppSettings settings = settingsManager.LoadSettings();
+
+            if (string.IsNullOrEmpty(settings.MusicFolderPath))
+            {
+                MessageBox.Show("Najpierw ustaw folder z muzyką w ustawieniach!", "Brak muzyki", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            SongManager songManager = new SongManager();
+            _songs = songManager.LoadSongsFromDirectory(settings.MusicFolderPath);
+
+            if (_songs.Count > 0)
+            {
+                PlayRandomSong();
+            }
+            else
+            {
+                MessageBox.Show("Nie znaleziono piosenek we wskazanym folderze. Sprawdź ustawienia.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void PlayRandomSong()
+        {
+            int index = _random.Next(_songs.Count);
+            _currentSong = _songs[index];
+
+            _mediaPlayer.Open(new Uri(_currentSong.FilePath));
+            _mediaPlayer.Play();
+
+            
+            MessageBox.Show($"Gramy: {_currentSong.DisplayInfo}", "Test Odtwarzacza");
         }
 
         private void BtnBack_Click(object sender, RoutedEventArgs e)
         {
+            
+            _mediaPlayer.Stop();
+
             var mainWindow = (MainWindow)Application.Current.MainWindow;
             mainWindow.ChangeView(new MenuView());
         }
