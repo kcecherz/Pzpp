978cbb63ec88b4905c2cda83b9388d4f489091a3
kcecherz <s61276@student.ubb.edu.pl>
2026-04-20 23:28:38 +0200

    Dodanie testów dla klasy Song i Profile

---
+++ Jaka_to_melodia.Tests/ProfileTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Jaka_to_melodia;
+
+namespace Jaka_to_melodia.Tests
+{
+    public class ProfileTests
+    {
+        [Fact]
+        public void NewProfile_ShouldHaveZeroHighscore_And_GeneratedId()
+        {
+            Profile newPlayer = new Profile("Kacper");
+
+            Assert.Equal("Kacper", newPlayer.Name);
+            Assert.Equal(0, newPlayer.Highscore); 
+
+            Assert.False(string.IsNullOrEmpty(newPlayer.Id));
+        }
+    }
+}
